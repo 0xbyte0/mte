f599c339ebad4f0a1944505853ef5a94d5d803ee
0x0000 <0x0000@0000.000>
2023-10-26 00:07:46 +0100

    f

---
+++ config-overrides.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+module.exports = function override(config, env) {
+    config.resolve.fallback = {
+        url: require.resolve('url'),
+        fs: require.resolve('fs'),
+        assert: require.resolve('assert'),
+        crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        'process/browser': require.resolve('process/browser'),
+        buffer: require.resolve('buffer'),
+        stream: require.resolve('stream-browserify'),
+        zlib: require.resolve("browserify-zlib")
+    };
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    );
+
+    config.ignoreWarnings = [/Failed to parse source map/];
+    config.module.rules.push({
+        test: /\.(js|mjs|jsx)$/,
+        enforce: "pre",
+        loader: require.resolve("source-map-loader"),
+        resolve: {
+            fullySpecified: false,
+        },
+    });
+    return config;
+}
